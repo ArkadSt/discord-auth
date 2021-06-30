764fde295b053ce7d8647cf2531d11153badecd4
ArkadSt <arkadst@pm.me>
2021-06-30 23:53:56 +0300

    update to 1.17

--- src/main/java/org/arkadst/discordauth/DiscordAuthCommand.java
+++ src/main/java/org/arkadst/discordauth/DiscordAuthCommand.java
@@ -4,6 +4,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
 
 public class DiscordAuthCommand implements CommandExecutor {
 
@@ -14,7 +15,7 @@ public class DiscordAuthCommand implements CommandExecutor {
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
         if (args.length == 1) {
             switch (args[0]) {
                 case "reload":
